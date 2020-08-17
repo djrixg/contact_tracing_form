4c515b6fd8b452a59906ccc85375bca529b6339b
djrixg <35298775+djrixg@users.noreply.github.com>
2020-08-17 21:26:36 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
-const cacheName = "forOffline";
-const staticAssets = [
-	'/index.html',
-	'bootstrap.min.css',
-	'/jquery.min.js',
-	'/jquery-3.2.1.slim.min.js',
-	'/bootstrap.min.js',
-	'/qrcode.min.js',
-	'/manifest.webmanifest'
-];
+// const cacheName = "forOffline";
+// const staticAssets = [
+// 	'/index.html',
+// 	'bootstrap.min.css',
+// 	'/jquery.min.js',
+// 	'/jquery-3.2.1.slim.min.js',
+// 	'/bootstrap.min.js',
+// 	'/qrcode.min.js',
+// 	'/manifest.webmanifest'
+// ];
 
-self.addEventListener('install',async e=>{
-	const cache = await caches.open(cacheName);
-	await cache.addAll(staticAssets);
-	return self.skipWaiting();
-});
-self.addEventListener('activate',async e=>{
-	self.clients.claim();
-});
+// self.addEventListener('install',async e=>{
+// 	const cache = await caches.open(cacheName);
+// 	await cache.addAll(staticAssets);
+// 	return self.skipWaiting();
+// });
+// self.addEventListener('activate',async e=>{
+// 	self.clients.claim();
+// });
