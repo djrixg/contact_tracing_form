d8c9d9bdbe2184b7165aaa6e539df266bc953b70
djrixg <35298775+djrixg@users.noreply.github.com>
2020-08-17 17:00:57 +0800

    trial_sw

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-if('serviceWorker' in navigator){
-	alert("Press Options and Click Add to Homescreen to use this even offline.");
+async function registerSW(){
+	if('serviceWorker' in navigator){
+		// alert("Press Options and Click Add to Homescreen to use this even offline.");
+		navigator.serviceWorker.register("sw.js")
+		.then((reg) => console.log("service worker registered\nReg:",reg))
+		.catch((err) => console.log("service worker not registered\nErr:",err));
+	}
 }
